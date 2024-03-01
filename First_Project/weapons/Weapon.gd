d5a562c8b31b0519af68de533776207e8817877f
E-Mello <e.mello.dev@gmail.com>
2024-03-01 14:25:56 -0400

    The bullet problem has been fixed, however, the player can now kill his allies, in this case the bug will become a feature until there is divine enlightenment for the solution

--- First_Project/weapons/Weapon.gd
+++ First_Project/weapons/Weapon.gd
@@ -17,6 +17,6 @@ func shoot():
 	if attack_cooldown.is_stopped() and Bullet != null:
 		var bullet_instance = Bullet.instance()
 		var direction = (gun_direction.global_position - end_of_gun.global_position).normalized()
-		GlobalSignals.emit_signal("bullet_fired", team, bullet_instance, end_of_gun.global_position, direction)
+		GlobalSignals.emit_signal("bullet_fired", bullet_instance, team, end_of_gun.global_position, direction)
 		attack_cooldown.start()
 		animation_player.play("muzzle_flash")
