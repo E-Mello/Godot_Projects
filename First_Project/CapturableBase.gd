efdd32e332ae7baf663f37ef96273f573b1ea06c
E-Mello <e.mello.dev@gmail.com>
2024-03-02 10:59:12 -0400

    Initialized the capturableBase implementation

---
+++ First_Project/CapturableBase.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+export (Color) var neutral_color = Color(1, 1, 1)
+export (Color) var player_color = Color(0.192157, 0.486275, 0.207843)
+export (Color) var enemy_color = Color(0.090196, 0.070588, 0.407843)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
