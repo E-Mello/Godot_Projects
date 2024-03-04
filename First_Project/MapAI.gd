2401b5014856fb840286348e94529be146dd1d33
E-Mello <e.mello.dev@gmail.com>
2024-03-04 08:52:33 -0400

    The base capture by the AI (both ally and enemy) has been completed, however, it is not working correctly, both allies and enemies are grouping together at some point on the map, in addition, the bug of killing allies continues, although, now I don't kill with just 1 hit, I need to do 5 haha, the only good thing is that enemies can no longer kill each other, that is, if they try to hit me and shoot their companion, they don't cause damage

--- First_Project/MapAI.gd
+++ First_Project/MapAI.gd
@@ -13,9 +13,17 @@ onready var team = $Team
 
 func initialize(capturable_bases: Array):
 	self.capturable_bases = capturable_bases
-	var next_base = get_next_capturable_base()
-	assign_next_capturable_base_to_units(next_base)
+	for base in capturable_bases:
+		base.connect("base_captured", self, "handle_base_captured")
+	check_for_capturable_bases()
 
+func handle_base_captured(_new_team: int):
+	check_for_capturable_bases()
+
+func check_for_capturable_bases():
+	var next_base = get_next_capturable_base()
+	if next_base != null and next_base != Vector2.ZERO:
+		assign_next_capturable_base_to_units(next_base)
 
 func get_next_capturable_base():
 	# Asume base capture start order is first
@@ -25,15 +33,17 @@ func get_next_capturable_base():
 		
 	for i in list_of_bases:
 		var base: CapturableBase = capturable_bases[i]
-		if team.team == base.team.team:
+		if team.team != base.team.team:
+			print("Assigning team %d to capture base %d" % [team.team, i])
 			return base.global_position
 	return null
 
 func assign_next_capturable_base_to_units(base_location: Vector2):
-	if base_location == null or base_location == Vector2.ZERO:
-		return
-		
 	for unit in get_children():
 		if unit == team:
 			continue
+		
 		# assign capturable base target here
+		var ai: AI = unit.ai
+		ai.next_base = base_location
+		ai.set_state(AI.State.ADVANCE)
