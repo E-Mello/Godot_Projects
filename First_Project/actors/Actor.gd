aea7d89593f6c41dad6ada0650b26eb73b8110df
E-Mello <e.mello.dev@gmail.com>
2024-03-16 13:44:46 -0400

    Polish, Unit Counts, and Fade-in Transitions

--- First_Project/actors/Actor.gd
+++ First_Project/actors/Actor.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 	weapon.initialize(team.team)
 
 func rotate_toward(location: Vector2):
-	rotation = lerp(rotation, global_position.direction_to(location).angle(), 0.1)
+	rotation = lerp_angle(rotation, global_position.direction_to(location).angle(), 0.1)
 
 func velocity_toward(location: Vector2) -> Vector2:
 	return global_position.direction_to(location) * speed
