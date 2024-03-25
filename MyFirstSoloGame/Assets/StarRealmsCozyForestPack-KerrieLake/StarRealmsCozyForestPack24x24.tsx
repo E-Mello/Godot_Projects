f4aad637f8a88055cd059988b35cb4319185a8a5
E-Mello <e.mello.dev@gmail.com>
2024-03-25 17:08:08 -0400

    More items added to the world map and an attempt to make the rocks in the ocean animated

---
+++ MyFirstSoloGame/Assets/StarRealmsCozyForestPack-KerrieLake/StarRealmsCozyForestPack24x24.tsx
@@ -0,0 +1,164 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="CuteForestPack24x24" tilewidth="24" tileheight="24" tilecount="400" columns="20">
+ <image source="StarRealmsCozyForestPack24x24.png" width="480" height="480"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="10" duration="200"/>
+   <frame tileid="15" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="11" duration="200"/>
+   <frame tileid="16" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="7" duration="200"/>
+   <frame tileid="12" duration="200"/>
+   <frame tileid="17" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="8" duration="200"/>
+   <frame tileid="13" duration="200"/>
+   <frame tileid="18" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="9" duration="200"/>
+   <frame tileid="14" duration="200"/>
+   <frame tileid="19" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="20">
+  <animation>
+   <frame tileid="20" duration="200"/>
+   <frame tileid="25" duration="200"/>
+   <frame tileid="30" duration="200"/>
+   <frame tileid="35" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="22">
+  <animation>
+   <frame tileid="22" duration="200"/>
+   <frame tileid="27" duration="200"/>
+   <frame tileid="32" duration="200"/>
+   <frame tileid="37" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="23">
+  <animation>
+   <frame tileid="23" duration="200"/>
+   <frame tileid="28" duration="200"/>
+   <frame tileid="33" duration="200"/>
+   <frame tileid="38" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="24">
+  <animation>
+   <frame tileid="24" duration="200"/>
+   <frame tileid="29" duration="200"/>
+   <frame tileid="34" duration="200"/>
+   <frame tileid="39" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="40">
+  <animation>
+   <frame tileid="40" duration="200"/>
+   <frame tileid="45" duration="200"/>
+   <frame tileid="50" duration="200"/>
+   <frame tileid="55" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="41">
+  <animation>
+   <frame tileid="41" duration="200"/>
+   <frame tileid="46" duration="200"/>
+   <frame tileid="51" duration="200"/>
+   <frame tileid="56" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="42">
+  <animation>
+   <frame tileid="42" duration="200"/>
+   <frame tileid="47" duration="200"/>
+   <frame tileid="52" duration="200"/>
+   <frame tileid="57" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="43">
+  <animation>
+   <frame tileid="43" duration="200"/>
+   <frame tileid="48" duration="200"/>
+   <frame tileid="53" duration="200"/>
+   <frame tileid="58" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="44">
+  <animation>
+   <frame tileid="44" duration="200"/>
+   <frame tileid="49" duration="200"/>
+   <frame tileid="54" duration="200"/>
+   <frame tileid="59" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="308">
+  <animation>
+   <frame tileid="308" duration="200"/>
+   <frame tileid="311" duration="200"/>
+   <frame tileid="314" duration="200"/>
+   <frame tileid="317" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="309">
+  <animation>
+   <frame tileid="309" duration="200"/>
+   <frame tileid="312" duration="200"/>
+   <frame tileid="315" duration="200"/>
+   <frame tileid="318" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="310">
+  <animation>
+   <frame tileid="310" duration="200"/>
+   <frame tileid="313" duration="200"/>
+   <frame tileid="316" duration="200"/>
+   <frame tileid="319" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="328">
+  <animation>
+   <frame tileid="328" duration="200"/>
+   <frame tileid="331" duration="200"/>
+   <frame tileid="334" duration="200"/>
+   <frame tileid="337" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="329">
+  <animation>
+   <frame tileid="329" duration="200"/>
+   <frame tileid="332" duration="200"/>
+   <frame tileid="335" duration="200"/>
+   <frame tileid="338" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="330">
+  <animation>
+   <frame tileid="330" duration="200"/>
+   <frame tileid="333" duration="200"/>
+   <frame tileid="336" duration="200"/>
+   <frame tileid="339" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
--- MyFirstSoloGame/ui/pause_screen.gd
+++ MyFirstSoloGame/ui/pause_screen.gd
@@ -5,7 +5,7 @@ func _ready():
 	hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_pressed("pause"):
 		visible = true
 		get_tree().paused = true
